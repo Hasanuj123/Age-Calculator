52361667f8ad7750a814cf2400bdcb0211cc3d4a
Hasanuj123 <mdhasanujjaman001@gmail.com>
2022-12-15 22:53:55 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,6 @@
+
+
+let div=window.document.getElementsByClassName("Heading")
+div.addEventListener("click", function() {
+  alert("done")
+});
---
+++ raf/index.js
@@ -0,0 +1,10 @@
+// let div=window.document.getElementsByClassName("Heading")
+// div.addEventListener("click", function() {
+//   alert("done")
+// });
+// alert("hiii")
+
+
+let Heading=window.document.getElementById("Heading").addEventListener("click",function (){
+    alert("gggg")
+})
